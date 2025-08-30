213b8cf54093630ccc8d5c47d72124af2309cf56
okonomiy4k1 <okonomiy4k1okonomiy4k1@gmail.com>
2025-08-30 21:26:02 +0900

    メインページ
    メインページ

---
+++ payty.js
@@ -0,0 +1,8 @@
+const parties = {
+  "nihon-hoshutou": { name: "日本保守党", catchphrase: "日本を豊かに、強く——。", summary: "強い国家主義と保守的価値観を掲げる政党。" },
+  "sanseitou": { name: "参政党", catchphrase: "日本人ファースト", summary: "中道右派の立場で政策を提案する政党。" },
+  "genzei-tou": { name: "減税党", catchphrase: "税金を減らして国民の生活を豊かに", summary: "" },
+  "kokumin-minshutou": { name: "国民民主党", catchphrase: "手取りを増やす", summary: "" },
+  "reiwa-shinsengumi": { name: "れいわ新選組", catchphrase: "さっさと消費税廃止", summary: "" },
+  "jiyuu-minshutou": { name: "自由民主党", catchphrase: "ルールを守る", summary: "" }
+};
