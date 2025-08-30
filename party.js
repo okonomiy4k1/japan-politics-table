ffe2b63063c26bfd58633c57a14f221c43cdb218
okonomiy4k1 <okonomiy4k1okonomiy4k1@gmail.com>
2025-08-30 22:03:46 +0900

    あこれまじだわ

--- party.js
+++ party.js
@@ -1,8 +1,32 @@
 const parties = {
-  "nihon-hoshutou": { name: "日本保守党", catchphrase: "日本を豊かに、強く——。", summary: "強い国家主義と保守的価値観を掲げる政党。" },
-  "sanseitou": { name: "参政党", catchphrase: "日本人ファースト", summary: "中道右派の立場で政策を提案する政党。" },
-  "genzei-tou": { name: "減税党", catchphrase: "税金を減らして国民の生活を豊かに", summary: "" },
-  "kokumin-minshutou": { name: "国民民主党", catchphrase: "手取りを増やす", summary: "" },
-  "reiwa-shinsengumi": { name: "れいわ新選組", catchphrase: "さっさと消費税廃止", summary: "" },
-  "jiyuu-minshutou": { name: "自由民主党", catchphrase: "ルールを守る", summary: "" }
+  "nihon-hoshutou": {
+    name: "日本保守党",
+    catchphrase: "日本を豊かに、強く——。",
+    summary: "強い国家主義と保守的価値観を掲げる政党。"
+  },
+  "sanseitou": {
+    name: "参政党",
+    catchphrase: "日本人ファースト",
+    summary: "中道右派の立場で政策を提案する政党。"
+  },
+  "genzei-tou": {
+    name: "減税党",
+    catchphrase: "税金を減らして国民の生活を豊かに",
+    summary: "国民の税負担を減らし、生活の安定を目指す政党。"
+  },
+  "kokumin-minshutou": {
+    name: "国民民主党",
+    catchphrase: "手取りを増やす",
+    summary: "給与や社会保障を重視して国民の手取りを増やす政策を掲げる。"
+  },
+  "reiwa-shinsengumi": {
+    name: "れいわ新選組",
+    catchphrase: "さっさと消費税廃止",
+    summary: "低所得者支援や消費税廃止を掲げる新進政党。"
+  },
+  "jiyuu-minshutou": {
+    name: "自由民主党",
+    catchphrase: "ルールを守る",
+    summary: "安定した政権運営と保守的政策を行う大政党。"
+  }
 };
